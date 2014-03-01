d29e26d676b0961de51b81d22e979596d40f5472
Vagner Bessa <vagner.fisica@gmail.com>
2014-03-01 18:54:09 -0300

    1.0/01.03.2014:1853 commit

--- pplot.py
+++ pplot.py
@@ -2,15 +2,8 @@ from pylab import *
 import matplotlib.pyplot as plt
 import os, trim
 
-doc = \
-		'-------------------------------------------\n'\
-		'pplot class\n'\
-		'Developed by Vagner Bessa.\n'\
-		'Contact: vagner.fisica@gmail.com.\n\n'\
-		'Check README file for main informations.\n\n'\
-		'This module has the MIT License (MIT)\n'\
-		'Copyright (c) 2014 Vagner Bessa.\n'\
-		'-------------------------------------------\n'
+doc = open('documentation.txt','r').read()
+
 def lzeros(nzeros, idx):
 	s = ("%0" + str(nzeros) + "d")  % (idx)
 	return s
@@ -22,13 +15,33 @@ class pplot:
 	dataFolder = "dataFolder"
 	plotsFolder = "plotsFolder"
 	cwd = os.getcwd()
-	fig = plt.figure(figsize=(6,6), dpi=80)
+	
+	xDimPLot = 6
+	yDimPlot = 6
+	fig = plt.figure(figsize = (xDimPLot,yDimPlot), dpi = 80)
 	frameDim = [0.1,.1,.8,.8]
-	ax = fig.add_axes(frameDim)
-	yLabel = r'$cos(\theta)$'
+	pXY = fig.add_axes(frameDim)
+	yLabel = r'$y(x)$'
+	xLabel = r'$x$'
+	
+	dataname = 'dataname.dat'
+	plotname = 'sampleplotXY.png'
+	
+	X = []
+	Y = []
+	Z = []
+	
+	XY = []
+	XYZ = []
+	
+	matrix = []
+	
+	colorDict = {'pblue':'#025167'}
 	
 	#----------------------		
 	def __init__(self, dFolder = "dataFolder", pFolder = "plotsFolder"):
+		"""Create the folder to work in.
+		"""
 		if type(dFolder) is str:
 			pth = os.path.join(self.cwd,dFolder)
 			if os.path.exists(pth):
@@ -58,58 +71,101 @@ class pplot:
 
 	#----------------------				
 	def plotfname(self,f):
+		"""Return path to file 'f' in folder pplot.plotsFolder
+		"""
 		f = os.path.join(self.plotsFolder,f)
-		return f	
+		return f
+
+	#----------------------	
 	def datafname(self,f):
+		"""Return path to file 'f' in folder pplot.dataFolder
+		"""
 		f = os.path.join(self.dataFolder,f)
-		return f				
+		return f
+	
+	#----------------------	
+	def saveData(self,dataname,data):
+		"""Store data in file in pplot standar format:
+			x11 y12 z13 ...
+			x21 y22 z23 ... 
+			x31 y32 z33 ... 
+				.
+				. 
+				. 			
+		"""
+		np.savetxt(dataname,data,delimiter = '\t',newline='\n')	
+
+	#----------------------		
+	def sampleplotXY(self):
+		"""Plot with a sample data: y(x) = cos(x) in range [-2Pi,2Pi].
+		"""
+		self.dataname = self.datafname('sampledataXY.dat')
+		self.plotname = self.plotfname('sampleplotXY.png')
+		
+		print "Ploting with \'%s\'" %(dataname)
+		
+		self.X = linspace(-2*pi,2*pi,100,endpoint=True)
+		self.Y = cos(x)
+		
+		self.XY = np.array(zip(x,y),dtype=dtype)
+		
+		self.saveData(dataname,self.XY)
+		
+		self.pXY.plot(self.X, self.X, label = self.yLabel, color = self.colorDict['pblue'], lw = 2.0)
+		
+		self.pXY.legend(loc = 3)
+
+		xlim(x.min(),x.max())
+		ylim(y.min(),y.max())
+		
+		from math import floor, ceil
+		xini = int(ceil(x.min()))
+		xend = int(floor(x.max()))
+		yini = int(ceil(y.min()))
+		yend = int(floor(y.max()))				
+		xticks = linspace(xini,xend,5,endpoint=True)
+		yticks = linspace(yini,yend,5,endpoint=True)
+		self.pXY.set_xticks(xticks)
+		self.pXY.set_yticks(yticks)
+
+		self.pXY.set_xlabel(self.xLabel, fontsize = 18)
+		self.pXY.set_ylabel(self.yLabel, fontsize = 18)
+		self.pXY.set_title('Sample data')
+		
+		
+		self.fig.savefig(plotname, dpi = 100)
+		
+		print "Plot saved at \'%s\'" %(plotname)	
+						
 	#----------------------
 	def plotXY(self,fName = None,SHOW = False):
+		"""If no data is supplied, the sampleplotXY() method is executed.
+		"""
 		if fName is None:
-		
-			dataname = self.datafname('sampledataXY.dat')
-			plotname = self.plotfname("sampleplotXY.png")
-			
-			print "Ploting with \'%s\'" %(dataname)
-			
-			x = linspace(-2*pi,2*pi,100,endpoint=True)
-			y = cos(x)
-			
-			data = np.array(zip(x,y),dtype=dtype)
-			np.savetxt(dataname,data,delimiter = '\t',newline='\n')
-			
-			self.ax.plot(x,y,label = self.yLabel, color='#025167', lw = 2.0)
-			self.ax.legend(loc = 3)
-
-			xlim(x.min(),x.max())
-
-			xticks = linspace(-6,6,5,endpoint=True)
-			self.ax.set_xticks(xticks)
-
-			ylim(-1.0,1.0)
-
-			self.ax.set_xlabel(r'$\theta$', fontsize = 18)
-			self.ax.set_ylabel(r'$y$', fontsize = 18)
-			self.ax.set_title('Title')
-			
-			
-			self.fig.savefig(plotname,dpi=72)
-			
-			print "Plot saved at \'%s\'" %(plotname)
-			
+			self.sampleplotXY()
 			if SHOW:
-				show()		
+				show()
 				
 	#----------------------				
 	def showDoc(self):
+		"""Print pplot's doc string
+		"""
 		print trim.trim(doc)
-
+	def showDoc(self, f):
+		"""Usefull to print <methodname>.__doc__.
+		Example:
+		'myplot.showDoc(myplot.showDoc.__doc__)'
+		shows this very docs string without \\t, \\n or identation.
+		"""
+		print trim.trim(f)
 	#----------------------			
 	def saveDoc(self):
 		out = open("documentation.txt","w")
 		out.write(trim.trim(doc))			
 
-#plot = pplot()
-#plot.plotXY(SHOW = True)
-#fName = 'documentation.dat'
-#x.saveDoc(fName)
+a = pplot()
+#a.plotXY(SHOW = True)
+a.doc(a.showDoc().__doc__)
+
+
+
